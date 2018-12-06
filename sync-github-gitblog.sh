093198f194bda82a037be8208aa8d3fa10a9bdc3
hack0072008 <82806791@qq.com>
2018-12-06 16:25:28 +0800

    modify sync shell

--- sync-github-gitblog.sh
+++ sync-github-gitblog.sh
@@ -10,6 +10,21 @@ git pull
 cd /opt/src/github/my-gitblog
 /bin/cp -rf ./blog/* /var/www/gitblog/blog/
 
+#copy gitblog theme to local gitblog theme
+cd /opt/src/github/my-gitblog
+/bin/cp -rf ./theme/* /var/www/gitblog/theme/
+
+#copy gitblog static to local gitblog  static
+cd /opt/src/github/my-gitblog
+/bin/cp -rf ./static/* /var/www/gitblog/static/
+/bin/cp -rf ./*.ico /var/www/gitblog/
+/bin/cp -rf ./*.png /var/www/gitblog/
+/bin/cp -rf ./*.gif /var/www/gitblog/
+
+#copy gitblog config to local gitblog  config
+cd /opt/src/github/my-gitblog
+/bin/cp -rf ./conf.yaml /var/www/gitblog/
+
 #list local gitblog
 ls -ltrh /var/www/gitblog/blog
 
